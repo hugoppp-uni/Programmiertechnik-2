60a4dc83ccf6a6f1ed7324bc999381992f3cd75b
acs521 <hugo.protsch@haw-hamburg.de>
2020-04-21 15:32:14 +0200

    Kommentare

--- src/main/java/praktikum/aufgabe2H/Figur.java
+++ src/main/java/praktikum/aufgabe2H/Figur.java
@@ -1,12 +1,17 @@
 package praktikum.aufgabe2H;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Figur implements Comparable<Figur> {
+public class Figur implements Comparable<Figur>, Iterable<String> {
   private final String id;
+  /**
+   * Groesse in metern
+   */
   private final float groesse;
   private final Typ typ;
   private final Geschlecht geschlecht;
@@ -15,18 +20,6 @@ public class Figur implements Comparable<Figur> {
   private final String todestag;
   private final String haarfarbe;
   private final String name;
-
-  public float getGroesse() {
-    return groesse;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getId() {
-    return id;
-  }
 
   public Figur(String id,
                float groesse,
@@ -48,10 +41,17 @@ public class Figur implements Comparable<Figur> {
     this.name = name;
   }
 
+  /**
+   * generates List of Figur from JSON file
+   *
+   * @param path path to json file
+   * @return List of Figure
+   */
   public static List<Figur> generateFromJson(String path) {
     ArrayList<Figur> ls = new ArrayList<>();
     var JsonLs = JSONReader.getJSONObjectList(path, "docs");
-    JsonLs.forEach(o -> ls.add(new Figur(JSONReader.getSafeString(o, "_id"),
+    JsonLs.forEach(o -> ls.add(new Figur(
+        JSONReader.getSafeString(o, "_id"),
         parseSize(JSONReader.getSafeString(o, "height")),
         Typ.from((JSONReader.getSafeString(o, "race"))),
         Geschlecht.from(JSONReader.getSafeString(o, "gender")),
@@ -59,12 +59,21 @@ public class Figur implements Comparable<Figur> {
         JSONReader.getSafeString(o, "spouse"),
         JSONReader.getSafeString(o, "death"),
         JSONReader.getSafeString(o, "hair"),
-        JSONReader.getSafeString(o, "name"))));
+        JSONReader.getSafeString(o, "name")
+    )));
     return ls;
   }
 
+  /**
+   * Parses double out of String, must one of the following formats:
+   * \d\d[.]\dm
+   * \d\d\dcm
+   *
+   * @param s String to be parsed
+   * @return parsed double
+   */
   private static float parseSize(String s) {
-    Matcher matcher = Pattern.compile("\\d\\d[.]\\d[m]").matcher(s);
+    Matcher matcher = Pattern.compile("\\d\\d[.]\\dm").matcher(s);
     if (matcher.find()) {
       String res = (matcher.group());
       return Float.parseFloat(res.substring(0, res.length() - 1));
@@ -72,19 +81,29 @@ public class Figur implements Comparable<Figur> {
     matcher = Pattern.compile(("\\d{3}cm")).matcher(s);
     if (matcher.find()) {
       String res = (matcher.group());
-      return Float.parseFloat(
-          res.charAt(0) + "." + res.charAt(1) + res.charAt(2));
+      return Float.parseFloat(res.charAt(0) + "." + res.charAt(1) + res.charAt(2));
     }
     return 0f;
   }
 
+  public float getGroesse() {
+    return groesse;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
   @Override
   public String toString() {
-    return "Figur{" + "id='" + id + "'\n" + ", groesse=" + groesse + ", typ=" +
-           typ + ", geschlecht=" + geschlecht + ", geburtstag='" + geburtstag +
-           '\'' + ", partner='" + partner + '\'' + ", todestag='" + todestag +
-           '\'' + ", haarfarbe='" + haarfarbe + '\'' + ", name='" + name +
-           '\'' + "}";
+    return "Figur{" + "id='" + id + "'\n" + ", groesse=" + groesse + ", typ=" + typ +
+           ", geschlecht=" + geschlecht + ", geburtstag='" + geburtstag + '\'' + ", partner='" +
+           partner + '\'' + ", todestag='" + todestag + '\'' + ", haarfarbe='" + haarfarbe + '\'' +
+           ", name='" + name + '\'' + "}";
   }
 
   @Override
@@ -92,4 +111,19 @@ public class Figur implements Comparable<Figur> {
     return name.compareTo(o.name);
   }
 
+  @Override
+  public Iterator<String> iterator() {
+    return (Arrays.asList(new String[]{
+        name,
+        id,
+        Double.toString(groesse),
+        typ.toString(),
+        geschlecht.toString(),
+        geburtstag,
+        partner,
+        todestag,
+        haarfarbe
+    })).
+        iterator();
+  }
 }
