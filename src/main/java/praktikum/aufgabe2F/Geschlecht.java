53b7824cb849afa9cee96ae9d6326988e10e755c
act133 <Franziska.Todt@haw-hamburg.de>
2020-04-21 22:55:52 +0200

    aufgabe2F

---
+++ src/main/java/praktikum/aufgabe2F/Geschlecht.java
@@ -0,0 +1,28 @@
+/**
+ * Diese Datei ist Teil der Vorgabe zur Lehrveranstaltung Programmiermethodik 2 im Studiengang ITS der Hochschule
+ * für Angewandte Wissenschaften Hamburg von Prof. Philipp Jenke (Informatik)
+ */
+
+package praktikum.aufgabe2F;
+
+/**
+ * Typ für das Geschlecht: Männlich oder Weiblich
+ */
+public enum Geschlecht {
+
+    MAENNLICH, WEIBLICH, UNDEFINED;
+
+    /**
+     * Liefert die Konstante für einen String. Liefert null, wenn keine passende Konstante gefunden wurde.
+     */
+    public static Geschlecht from(String text) {
+        switch (text) {
+            case "Male":
+                return MAENNLICH;
+            case "Female":
+                return WEIBLICH;
+            default:
+                return null;
+        }
+    }
+}
